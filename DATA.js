e89067229fbaac7c97739a325394cf04ae089e23
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-01 10:09:16 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,55 @@
+export const item = {
+    "id": "76e337ac-c4be-44b6-848b-0a99e5bf2e11",
+    "main_title": "Most agency face around rather end soon. Before president conference suffer. Thought address explain case hair.",
+    "current_time": "No whose thousand accept suffer.",
+    "seat_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png",
+    "sort_by": "community",
+    "deal_score": "will",
+    "down_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png",
+    "seat_count": 256,
+    "seat_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png",
+    "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png",
+    "fees_title": "add",
+    "charges_list": [
+        {
+            "id": "f61485b1-8149-4777-9eac-633b6ebbd0fa",
+            "charges": 4495,
+            "seat_location": "Store wind left plant last day current. Most great effect law avoid. Know ten paper born explain different.",
+            "rating": "mother",
+            "amazing_deal": "total",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+        },
+        {
+            "id": "5074e8ba-b6e1-4248-b8bd-2a707156405c",
+            "charges": 7528,
+            "seat_location": "Second must family success front.",
+            "rating": "man",
+            "amazing_deal": "contain",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png"
+        },
+        {
+            "id": "1513d8ba-af48-49a3-b02e-4d468b67523d",
+            "charges": 3237,
+            "seat_location": "Child page rule benefit off. House bad share all bar. System industry painting fact usually.",
+            "rating": "we",
+            "amazing_deal": "than",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+        },
+        {
+            "id": "3fb4f852-7d7f-40fc-bde8-8a7642002ecd",
+            "charges": 7359,
+            "seat_location": "Me do central matter. Moment court service glass decision.",
+            "rating": "wall",
+            "amazing_deal": "answer",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+        },
+        {
+            "id": "215c2f46-72ae-4a3f-9b7c-2a5459d5c3fb",
+            "charges": 1446,
+            "seat_location": "Check idea thought. He class with bit.",
+            "rating": "report",
+            "amazing_deal": "heart",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+        }
+    ]
+}
